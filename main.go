25240a667f2336b4d886960904d394a6a0fe8c8a
MandavkarPranjal <programmer.pranjal@gmail.com>
2024-12-29 16:19:17 +0530

    feat: github-activity cli

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/MandavkarPranjal/github-user-activity/cmd"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	// rootCmd := cmd.NewRootCmd()
+
+	if err := rootCmd.Execute(); err != nil {
+		errorStyle := lipgloss.NewStyle().
+			// Foreground(lipgloss.Color("#FF0000")).
+			Padding(1, 2).
+			Bold(true).
+			Render(fmt.Sprintf("Error: %s", err))
+		fmt.Println(errorStyle)
+	}
+}
